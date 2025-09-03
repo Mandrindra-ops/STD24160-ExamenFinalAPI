b79de86b5650199b76bcd54b44a88985d879df9c
Mandrindra <hei.mandrindra@gmail.com>
2025-09-03 09:02:27 +0200

    update

--- main.py
+++ main.py
@@ -33,15 +33,6 @@ async def get_phone(id: str):
         return {"phone": phone}
     return {"error": "Phone not found"}, 404
 
-@app.put("PUT /phones/{id}/characteristics")
-async def update_characteristics(id: str, characteristics={
-    "ram_memory": int,
-    "rom_memory": int,
-}):
-    phone = next((phone for phone in get_phones()["liste_phones"] if phone["id"] == id), None)
-    if phone:
-        phone["characteristics"] = characteristics
-        return {"phone": phone}
-    return {"error": "Phone not found"}, 404
 
-    
+@app.put("/phones/{id}/characteristics")
+a
