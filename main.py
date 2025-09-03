5dceae136253c7a3485193a42b43820c8a9800b8
Mandrindra <hei.mandrindra@gmail.com>
2025-09-03 09:00:15 +0200

    update

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ async def ping():
 async def health():
     return {"texte": "ok"}
 
-@app.post("/phone")
+@app.post("/phones")
 async def create_phone(id: str, brand: str, model: str, characteristics={
     "ram_memory": int,
     "rom_memory": int,
@@ -20,3 +20,28 @@ async def create_phone(id: str, brand: str, model: str, characteristics={
              "brand": brand,
              "model": model,
              "characteristics": characteristics}
+
+@app.get("/phones")
+async def get_phones():
+    return {"liste_phones": [create_phone]}
+
+
+@app.get("/phones/{id}")
+async def get_phone(id: str):
+    phone = next((phone for phone in get_phones()["liste_phones"] if phone["id"] == id), None)
+    if phone:
+        return {"phone": phone}
+    return {"error": "Phone not found"}, 404
+
+@app.put("PUT /phones/{id}/characteristics")
+async def update_characteristics(id: str, characteristics={
+    "ram_memory": int,
+    "rom_memory": int,
+}):
+    phone = next((phone for phone in get_phones()["liste_phones"] if phone["id"] == id), None)
+    if phone:
+        phone["characteristics"] = characteristics
+        return {"phone": phone}
+    return {"error": "Phone not found"}, 404
+
+    
